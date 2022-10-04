5a35631b4b221bc54578c4df2d22459cc9948578
jiangkeer-art <130060060@qq.com>
2022-10-04 16:06:44 +0800

    asd

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" >
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Login from "@/views/Login";
+export default {
+  name: 'app',
+  components: {
+    Login,
+  }
+
+}
+</script>
+
+<style>
+#app{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: -8px -8px auto;
+}
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="main">
+    <div class="left">
+      <h3>------------------------------------------------------------------------</h3>
+      基于springboot和vue的宿舍管理系统
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.main {
+  width: 100%;
+  height: 90%;
+  display: flex;
+}
+
+.left {
+  width: 300px;
+  height: 100%;
+  overflow-y: auto;
+}
+
+.right {
+  flex: 1;
+  height: 100%;
+  overflow-y: auto;
+  border-radius: 20px;
+  background-image: linear-gradient( 135deg, #EE9AE5 10%, #5961F9 100%);
+}
+
+.img{
+  width:300px;
+  height:400px;
+}
+
+</style>
